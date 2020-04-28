20cfa0a30616ae1ed1d420f3573e5cc9bcdd4bb7
Pierre Jego <pierre.jego@jdev.fr>
2020-04-28 18:10:24 +0200

    Update with GIP-ATEGERI new informations

---
+++ addons/bootstraptable/js/addbootstraptable.js
@@ -0,0 +1,10 @@
+var cssbootstraptable = document.createElement("link");
+	cssbootstraptable.setAttribute('rel', 'stylesheet');
+	cssbootstraptable.type = 'text/css';
+	cssbootstraptable.href = "https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.css";
+	document.head.appendChild(cssbootstraptable);
+
+var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.js";
+		document.body.appendChild(script);
---
+++ addons/styleinfo/js/addstyleinfo.js
@@ -0,0 +1,5 @@
+var styleinfocss = document.createElement("link");
+	styleinfocss.setAttribute('rel', 'stylesheet');
+	styleinfocss.type = 'text/css';
+	styleinfocss.href = "apps/donneesplages/addons/styleinfo/css/styleinfo.css";
+	document.body.appendChild(styleinfocss);
