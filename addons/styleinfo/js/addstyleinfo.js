ec86e243d6a929703c61deeb5a27b1d1a2fdb82a
Pierre Jego <pierre.jego@jdev.fr>
2020-04-29 18:11:45 +0200

    change addons management

--- addons/styleinfo/js/addstyleinfo.js
+++
@@ -1,5 +0,0 @@
-var styleinfocss = document.createElement("link");
-	styleinfocss.setAttribute('rel', 'stylesheet');
-	styleinfocss.type = 'text/css';
-	styleinfocss.href = "apps/donneesplages/addons/styleinfo/css/styleinfo.css";
-	document.body.appendChild(styleinfocss);
